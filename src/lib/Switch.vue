f4d78644c49c287569a7ba88fa350fd84064d703
weycy <wcy1015@126.com>
2022-11-28 19:47:32 +0800

    动画效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -36,6 +36,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 /2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
@@ -43,5 +44,9 @@ button.checked {
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
