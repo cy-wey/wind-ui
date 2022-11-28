dac1bc324f64ca67be58c631bda94a68269a7767
weycy <wcy1015@126.com>
2022-11-28 17:20:33 +0800

    初始aside的html

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './view/Home.vue'
 import Doc from './view/Doc.vue'
+import './index.scss'
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
