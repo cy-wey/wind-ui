04c35985c39be651d3ce3670ef23cfae33210a72
weycy <wcy1015@126.com>
2022-12-05 22:00:53 +0800

    Dialog css1

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,26 @@
 <template>
 <h3>Dialog示例</h3>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x"></Dialog>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue';
+import Dialog from '../lib/Dialog.vue';
+import {ref} from 'vue'
 export default {
   name: "DialogDemo",
   components: {
-    Dialog
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value;
+    }
+    return {x, toggle}
   }
 }
 </script>
