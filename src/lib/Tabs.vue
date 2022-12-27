a5275374518f4a376f7b2432f38b3ee20942eea9
weycy <wcy1015@126.com>
2022-12-27 11:34:35 +0800

    显示Tab

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,30 @@
 <template>
-
+<div>
+  Tabs组件
+  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+  <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+</div>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from './Tab.vue'
 export default {
-  name: "Tabs"
+  name: "Tabs",
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 的子标签必需是 Tab')
+      }
+    })
+   const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return {
+      defaults,
+      titles
+    }
+  }
 }
 </script>
 
