e013167065b4f48928b0a246ff6a097e2f747014
weycy <wcy1015@126.com>
2022-12-28 14:45:18 +0800

    填充文档内容

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from "./router";
 import './index.scss'
+import 'github-markdown-css'
 
 const app = createApp(App)
 app.use(router)
