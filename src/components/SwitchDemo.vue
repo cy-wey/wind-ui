7895bd0d8f384fca91e6bd44d314f926093b5b05
weycy <wcy1015@126.com>
2022-12-29 12:27:08 +0800

    删除无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,26 +7,18 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue';
 import Switch2Demo from './Switch2.demo.vue';
 import Switch1Demo from './Switch1.demo.vue';
-import Switch from "../lib/Switch.vue";
-import {ref} from 'vue';
 import Demo from './Demo.vue'
 
 export default {
-  name: "SwitchDemo",
   components: {
-    Button,
     Demo
   },
   setup() {
-    const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism
     }
   }
 }
