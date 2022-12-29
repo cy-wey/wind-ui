6e0c52aaf85397340553dd9668a7bbff6c3a4af7
weycy <wcy1015@126.com>
2022-12-29 12:22:56 +0800

    封装Demo组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import fs from 'fs'
 import {baseParse} from '@vue/compiler-core'
 export default {
@@ -5,7 +6,9 @@ export default {
     demo: (options) => {
       const { code, path } = options
       const file = fs.readFileSync(path).toString()
+      // @ts-ignore
       const parsed = baseParse(file).children.find(n => n.tag === 'demo')
+      // @ts-ignore
       const title = parsed.children[0].content
       const main = file.split(parsed.loc.source).join('').trim()
       return `export default function (Component) {
