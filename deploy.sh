ba2b5dd9b00d138af836aadc7f848337d1083726
weycy <wcy1015@126.com>
2022-12-29 19:26:22 +0800

    一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M main &&
+git remote add origin git@github.com:cy-wey/wind-ui-website.git &&
+git push -f -u origin main &&
+cd -
+echo https://cy-wey.github.io/wind-ui-website/index.html
