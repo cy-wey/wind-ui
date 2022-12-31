e563a6e5318c76ddf28c6455ba1ca4eb43ce5d59
weycy <wcy1015@126.com>
2022-12-31 14:26:50 +0800

    一键部署代码更新

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,11 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-<<<<<<< HEAD
 git branch -M main &&
 git remote add origin git@github.com:cy-wey/wind-ui-website.git &&
 git push -f -u origin main &&
-=======
 git branch -M master &&
 git remote add origin git@github.com:cy-wey/wind-ui-website.git &&
 git push -f -u origin master &&
->>>>>>> origin/main
 cd -
 echo https://cy-wey.github.io/wind-ui-website/index.html
