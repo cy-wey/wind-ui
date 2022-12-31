f9f8cc5d0a910d04f372ca530b476e0e26690b27
weycy <wcy1015@126.com>
2022-12-31 16:33:29 +0800

    修复 rollup 配置后不能 yarn dev 的 bug

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -23,6 +23,7 @@ export default {
 </script>
 
 <style lang="scss">
+@use "sass:math";
 $h: 22px;
 $h2: $h - 4px;
 .gulu-switch {
@@ -30,7 +31,7 @@ $h2: $h - 4px;
   width: $h*2;
   border: none;
   background: gray;
-  border-radius: $h/2;
+  border-radius: math.div($h, 2);
   position: relative;
 
   span {
@@ -40,7 +41,7 @@ $h2: $h - 4px;
     height: $h2;
     width: $h2;
     background: white;
-    border-radius: $h2 /2;
+    border-radius:math.div($h2, 2);
     transition: left 250ms;
   }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 请注意，当前文件的后缀从 .js 改为了 .ts
+// 如果你看到这行注释，请确认文件后缀是 .ts
+// 然后就可以删掉本注释了!!!!!!!!!!!!!!!!
+
 // @ts-ignore
-import fs from 'fs'
-import {baseParse} from '@vue/compiler-core'
+import { md } from "./plugins/md";
+import { demo } from "./plugins/demo";
+import vue from '@vitejs/plugin-vue'
+
 export default {
   base: './',
   assetsDir: 'assets',
-  vueCustomBlockTransforms: {
-    demo: (options) => {
-      const { code, path } = options
-      const file = fs.readFileSync(path).toString()
-      // @ts-ignore
-      const parsed = baseParse(file).children.find(n => n.tag === 'demo')
-      // @ts-ignore
-      const title = parsed.children[0].content
-      const main = file.split(parsed.loc.source).join('').trim()
-      return `export default function (Component) {
-        Component.__sourceCode = ${
-        JSON.stringify(main)
-      }
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
-      }`.trim()
-    }
-  }
+  plugins: [vue(), demo(), md()],
 };
